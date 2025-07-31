9db262eacef8c5befea03343adafdff06f749056
htoll <135985431+htoll@users.noreply.github.com>
2025-07-31 15:13:21 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -160,7 +160,7 @@ def gaussian(x, amp, mu, sigma):
 def plot_brightness(image_data_cps, df, show_fits = True, save_as_svg = False, plot_brightness_histogram = False, normalization = None, pix_size_um = 0.1):
 
     fig_width, fig_height = 6, 6
-    scale = fig_width / 2  
+    scale = fig_width / 10  
 
     fig, ax = plt.subplots(figsize=(fig_width, fig_height))
     im = ax.imshow(image_data_cps + 1, cmap='magma', norm=normalization, origin='lower') #LogNorm()
