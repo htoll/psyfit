60c0c97bd006f70e195ce3174195f3fbdda08514
htoll <135985431+htoll@users.noreply.github.com>
2025-07-31 15:24:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ tool = st.sidebar.radio("Select a tool:", [
     "Batch Convert SIFs",
     "Visualize Data"
 ])
-col1, col2 = st.columns([1, 4])
+col1, col2 = st.columns([1, 2])
 
 # Tool: Analyze single SIF
 if tool == "Analyze single SIF":
