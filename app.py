e450d8d874e48c525a98a8d30a28a1c50cc4dd1c
htoll <135985431+htoll@users.noreply.github.com>
2025-08-05 13:19:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import io
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
-from tools import analyze_single_sif, batch_convert#, analyze_colocalization, batch_convert, visualize_data
+from tools import analyze_single_sif, batch_convert, colocalization
 import sif_parser
 
 from skimage.feature import peak_local_max
