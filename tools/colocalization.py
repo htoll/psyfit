c40ba5a5f8793e1ab013bb2e968e3dc29bff4220
htoll <135985431+htoll@users.noreply.github.com>
2025-08-05 16:26:38 -0400

    Update colocalization.py

--- tools/colocalization.py
+++ tools/colocalization.py
@@ -55,18 +55,25 @@ def run():
           st.session_state.convert = True
       
       if st.session_state.convert and uploaded_files:
-          try:
-              ucnp_list, dye_list = sort_UCNP_dye_sifs(uploaded_files, ucnp_id=ucnp_id, dye_id=dye_id)
-              df_dict = {}
-            # Process UCNP files
-              for file in ucnp_list:
+          # Process UCNP files
+          for file in ucnp_list:
+              try:
                   df, cropped_img = integrate_sif(file, threshold=ucnp_threshold, region=ucnp_region, signal='UCNP')
-                  df_dict[file] = (df, cropped_img)
-              
-              # Process dye  files
-              for file in dye_list:
+                  if df is not None:
+                      df_dict[file] = (df, cropped_img)
+              except Exception as e:
+                  st.error(f"Could not process UCNP file: {os.path.basename(file)}")
+                  st.exception(e)
+  
+          # Process dye files
+          for file in dye_list:
+              try:
                   df, cropped_img = integrate_sif(file, threshold=dye_threshold, region=dye_region, signal='dye')
-                  df_dict[file] = (df, cropped_img)
+                  if df is not None:
+                      df_dict[file] = (df, cropped_img)
+              except Exception as e:
+                  st.error(f"Could not process dye file: {os.path.basename(file)}")
+                  st.exception(e)
 
             
               processed_data, combined_df = process_files(uploaded_files, region)
