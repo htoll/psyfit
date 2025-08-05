a41786ea19299c51a0fd759f9d042647d91d0f69
htoll <135985431+htoll@users.noreply.github.com>
2025-08-05 09:48:17 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -605,6 +605,10 @@ def plot_all_sifs(sif_files, df_dict, colocalization_radius=2, show_fits=True, n
         # Only show colorbar on the last subplot in the first row (column n_cols-1)
         if not univ_minmax:
             plt.colorbar(im, ax=ax, label='pps', fraction=0.046, pad=0.04)
+        if univ_minmax:
+            colorbar_ax = axes[min(n_cols - 1, n_files - 1)]
+            im = colorbar_ax.images[0]  # Get the image from that subplot
+            plt.colorbar(im, ax=colorbar_ax, label='pps', fraction=0.046, pad=0.04)
 
         basename = os.path.basename(sif_name)
         match = re.search(r'(\d+)\.sif$', basename)
