37ad46fac5eff9689bb518f632af0f84a51b5037
htoll <135985431+htoll@users.noreply.github.com>
2025-08-05 16:35:44 -0400

    Update colocalization.py

--- tools/colocalization.py
+++ tools/colocalization.py
@@ -69,7 +69,7 @@ def run():
                   if df is not None:
                       df_dict[file] = (df, cropped_img)
               except Exception as e:
-                st.error(f"Could not process UCNP file: {os.path.basename(file)}")
+                st.error(f"Could not process UCNP file: {file.name}")
                 st.exception(e)
 
           # Process dye files
@@ -79,7 +79,7 @@ def run():
                   if df is not None:
                       df_dict[file] = (df, cropped_img)
               except Exception as e:
-                  st.error(f"Could not process dye file: {os.path.basename(file)}")
+                  st.error(f"Could not process dye file: {file.name}"")
                   st.exception(e)
 
             
