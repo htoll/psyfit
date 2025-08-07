54e66088a0fddc1af238c713b54082a9dda1525b
htoll <135985431+htoll@users.noreply.github.com>
2025-08-06 20:42:13 -0400

    Update monomers.py

--- tools/monomers.py
+++ tools/monomers.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 import io
-from utils import integrate_sif, plot_brightness, plot_histogram
+from utils import integrate_sif, plot_brightness, plot_histogram, HWT_aesthetic
 from tools.process_files import process_files
 from matplotlib.colors import LogNorm
 import numpy as np
