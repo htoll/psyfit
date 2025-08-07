0ebcf4894fd495b01e1775357e487fafe3a22e8f
htoll <135985431+htoll@users.noreply.github.com>
2025-08-06 20:57:39 -0400

    Update analyze_single_sif.py

--- tools/analyze_single_sif.py
+++ tools/analyze_single_sif.py
@@ -7,7 +7,7 @@ from matplotlib.colors import LogNorm
 import numpy as np
 
 def run():
-    col1, col2 = st.columns([1, 2])
+    #col1, col2 = st.columns([1, 2])
 
     with col1:
         st.header("Analyze SIF Files")
@@ -49,81 +49,80 @@ def run():
                 processed_data, combined_df = process_files(uploaded_files, region)
 
                 # Now create the sub-columns for the plots below the selectbox
-                plot_col1, plot_col2 = st.columns(2)
+                #plot_col1, plot_col2 = st.columns(2)
 
-                with plot_col1:
-                    if len(uploaded_files) > 1:
-                        file_options = [f.name for f in uploaded_files]
-                        selected_file_name = st.selectbox("Select sif to display:", options=file_options)
-                    else:
-                        selected_file_name = uploaded_files[0].name  # Default to the only file's name
-                    if selected_file_name in processed_data:
-                        data_to_plot = processed_data[selected_file_name]
-                        df_selected = data_to_plot["df"]
-                        image_data_cps = data_to_plot["image"]
-                    normalization_to_use = LogNorm() if normalization else None
-                    fig_image = plot_brightness(
-                        image_data_cps,
-                        df_selected,
-                        show_fits=show_fits,
-                        normalization=normalization_to_use,
-                        pix_size_um=0.1,
-                        cmap = cmap
-                    )
-                    st.pyplot(fig_image)
+                #with plot_col1:
+                if len(uploaded_files) > 1:
+                    file_options = [f.name for f in uploaded_files]
+                    selected_file_name = st.selectbox("Select sif to display:", options=file_options)
+                else:
+                    selected_file_name = uploaded_files[0].name  # Default to the only file's name
+                if selected_file_name in processed_data:
+                    data_to_plot = processed_data[selected_file_name]
+                    df_selected = data_to_plot["df"]
+                    image_data_cps = data_to_plot["image"]
+                normalization_to_use = LogNorm() if normalization else None
+                fig_image = plot_brightness(
+                    image_data_cps,
+                    df_selected,
+                    show_fits=show_fits,
+                    normalization=normalization_to_use,
+                    pix_size_um=0.1,
+                    cmap = cmap
+                )
+                st.pyplot(fig_image)
 
-                    svg_buffer = io.StringIO()
-                    fig_image.savefig(svg_buffer, format='svg')
-                    svg_data = svg_buffer.getvalue()
-                    svg_buffer.close()
-                    st.download_button(
-                        label="Download PSFs",
-                        data=svg_data,
-                        file_name=f"{selected_file_name}.svg",
-                        mime="image/svg+xml"
-                    )
+                svg_buffer = io.StringIO()
+                fig_image.savefig(svg_buffer, format='svg')
+                svg_data = svg_buffer.getvalue()
+                svg_buffer.close()
+                st.download_button(
+                    label="Download PSFs",
+                    data=svg_data,
+                    file_name=f"{selected_file_name}.svg",
+                    mime="image/svg+xml"
+                )
                     
                 if plot_brightness_histogram and not combined_df.empty:
-                    with plot_col2:                
-                        # Auto-detect full data range
-                        brightness_vals = combined_df['brightness_fit'].values
-                        default_min = float(np.min(brightness_vals))
-                        default_max = float(np.max(brightness_vals))
-                
-                        # User inputs
-                        default_min_val = float(np.min(brightness_vals))
-                        default_max_val = float(np.max(brightness_vals))
-                        user_min_val_str = st.text_input("Min Brightness (pps)", value=f"{default_min_val:.2e}")
-                        user_max_val_str = st.text_input("Max Brightness (pps)", value=f"{default_max_val:.2e}")
-                        try:
-                            user_min = float(user_min_val_str)
-                            user_max = float(user_max_val_str)
-                        except ValueError:
-                            st.warning("Please enter valid numbers (you can use scientific notation like 1e6).")
-                            return  # or skip processing
+                   # with plot_col2:                
+                    # Auto-detect full data range
+                    brightness_vals = combined_df['brightness_fit'].values
+                    default_min = float(np.min(brightness_vals))
+                    default_max = float(np.max(brightness_vals))
+                    
+                    # User inputs
+                    default_min_val = float(np.min(brightness_vals))
+                    default_max_val = float(np.max(brightness_vals))
+                    user_min_val_str = st.text_input("Min Brightness (pps)", value=f"{default_min_val:.2e}")
+                    user_max_val_str = st.text_input("Max Brightness (pps)", value=f"{default_max_val:.2e}")
+                    try:
+                        user_min = float(user_min_val_str)
+                        user_max = float(user_max_val_str)
+                    except ValueError:
+                        st.warning("Please enter valid numbers (you can use scientific notation like 1e6).")
+                        return  # or skip processing
+                    
+                    #binning
+                    num_bins = st.number_input("# Bins:", value = 20)
+                    
+                    if user_min < user_max:
+                        fig_hist, _, _= plot_histogram(combined_df, min_val=user_min, max_val=user_max, num_bins = num_bins)
+                        st.pyplot(fig_hist)
+                    svg_buffer_hist = io.StringIO()
+                    fig_hist.savefig(svg_buffer_hist, format='svg')
+                    svg_data_hist = svg_buffer_hist.getvalue()
+                    svg_buffer_hist.close()
+        
+                    st.download_button(
+                        label="Download histogram",
+                        data=svg_data_hist,
+                        file_name="combined_histogram.svg",
+                        mime="image/svg+xml"
+                    )
 
-                        #binning
-                        num_bins = st.number_input("# Bins:", value = 20)
                 
-                        if user_min < user_max:
-                            fig_hist, _, _= plot_histogram(combined_df, min_val=user_min, max_val=user_max, num_bins = num_bins)
-                            st.pyplot(fig_hist)
-                
-                            svg_buffer_hist = io.StringIO()
-                            fig_hist.savefig(svg_buffer_hist, format='svg')
-                            svg_data_hist = svg_buffer_hist.getvalue()
-                            svg_buffer_hist.close()
-                
-                            st.download_button(
-                                label="Download histogram",
-                                data=svg_data_hist,
-                                file_name="combined_histogram.svg",
-                                mime="image/svg+xml"
-                            )
-
-                        
-                        else:
-                            st.warning("Min greater than max")
+                else:
+                    st.warning("Min greater than max")
 
                 else:
                     st.error(f"Data for file '{selected_file_name}' not found.")
