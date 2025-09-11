9618525654e2e01e48d179f547592364130cfe7a
jfshida <128092672+jfshida@users.noreply.github.com>
2025-09-11 15:13:42 -0400

    Update SaturationSeries.py

--- tools/SaturationSeries.py
+++ tools/SaturationSeries.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 import io
 from utils import integrate_sif, plot_brightness, plot_histogram
-from tools.process_files import process_files
+from utilsJFS import process_files
 from matplotlib.colors import LogNorm
 import numpy as np
 from scipy.ndimage import gaussian_filter
