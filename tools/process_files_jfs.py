f94257c20f30724382c6c2ea779c2d101a55c7a8
jfshida <128092672+jfshida@users.noreply.github.com>
2025-09-15 10:42:54 -0400

    Update process_files_jfs.py

--- tools/process_files_jfs.py
+++ tools/process_files_jfs.py
@@ -25,7 +25,7 @@ def process_files(uploaded_files, region, threshold=1, signal="UCNP", pix_size_u
             f.write(uploaded_file.getbuffer())
         
         try:
-            df, image_data_cps = integrate_sif(file_path, 
+            df, image_data_cps = integrate_sif_Center(file_path, 
                                                region=region,
                                                   threshold=threshold,
                                                signal = signal,
