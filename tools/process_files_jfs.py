dc04611c62edc766f9ed5cbe0a915d08e3fdd850
jfshida <128092672+jfshida@users.noreply.github.com>
2025-09-16 09:51:01 -0400

    Update process_files_jfs.py

--- tools/process_files_jfs.py
+++ tools/process_files_jfs.py
@@ -4,11 +4,10 @@ import pandas as pd
 from matplotlib.colors import LogNorm
 import io
 from utils import integrate_sif, plot_brightness, plot_histogram 
-from utilsJFS import integrate_sif_Center
 
 
 @st.cache_data
-def process_files_jfs(uploaded_files, region, threshold=1, signal="UCNP", pix_size_um=0.1, sig_threshold=0.3):
+def process_files(uploaded_files, region, threshold=1, signal="UCNP", pix_size_um=0.1, sig_threshold=0.3):
     """
     Processes all uploaded .sif files and returns a dictionary of dataframes
     and images, plus a single combined dataframe for the histogram.
@@ -19,13 +18,14 @@ def process_files_jfs(uploaded_files, region, threshold=1, signal="UCNP", pix_si
     # Use a temporary directory to store files
     temp_dir = "temp"
     os.makedirs(temp_dir, exist_ok=True)
+    
     for uploaded_file in uploaded_files:
         file_path = os.path.join(temp_dir, uploaded_file.name)
         with open(file_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
         
         try:
-            df, image_data_cps = integrate_sif_Center(file_path, 
+            df, image_data_cps = integrate_sif(file_path, 
                                                region=region,
                                                   threshold=threshold,
                                                signal = signal,
